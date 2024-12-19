f9768cfafdd807f3fd3df2f0813230294edc33dc
bsmsoma <soma.bsm@gmail.com>
2024-12-19 14:01:39 -0300

    Update Quotermachine.jsx

--- src/components/Quotermachine.jsx
+++ src/components/Quotermachine.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
 
 
-function Quotermachine(props) {
+function Quotermachine() {
     //Link da api with quotes;
     const api = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
 
